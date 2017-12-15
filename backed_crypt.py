93c8dcf5c653ac0513c2dace9f5e7ed6f65d21e7
guntuku.shiva <guntuku.shiva@digitalerp.xyz>
2017-12-15 09:14:07 +0530

    server up

--- backed_crypt.py
+++ backed_crypt.py
@@ -1,21 +1,40 @@
-import requests
+
 import csv
 from datetime import datetime as d
+import requests
+import time
+
 
 currecy_list = ['bitcoin', 'ethereum',
                 'ripple', 'litecoin']
-for currency in currecy_list:
-    url = 'https://api.coinmarketcap.com/\
+notify_dict = {'bitcoin': 1, 'ethereum': 2,
+               'ripple': 3, 'litecoin': 4}
+final_val = {'bitcoin': 1, 'ethereum': 2,
+             'ripple': 3, 'litecoin': 4}
+per_dict = {'bitcoin': 1.27,
+            'ethereum': 1.323,
+            'ripple': 1.35,
+            'litecoin': 1.32}
+
+while True:
+    print("running....")
+    for currency in currecy_list:
+        url = 'https://api.coinmarketcap.com/\
 v1/ticker/{0}/?convert=INR'.format(currency)
-    request = requests.get(url)
-    request_json = request.json()
-    for crypto in request_json:
-        csv_file = crypto['id'] + '_' + d.today().strftime('%d-%m-%Y')
-        path = '{0}/{1}'.format('backed_crypto', csv_file)
-        with open(path, 'a') as f:
-            w = csv.DictWriter(f, crypto.keys())
-            if f.tell() == 0:
-                w.writeheader()
-                w.writerow(crypto)
-            else:
-                w.writerow(crypto)
+        request = requests.get(url)
+        request_json = request.json()
+        for crypto in request_json:
+            notify_dict[crypto['id']] = crypto['price_inr']
+            mul = float(notify_dict[crypto['id']]) * per_dict[crypto['id']]
+            final_val[crypto['id']] = mul
+            csv_file = crypto['id'] + '_' + d.today().strftime('%d-%m-%Y')
+            path = '{0}/{1}'.format('backed_crypto', csv_file)
+            with open(path, 'a') as f:
+                w = csv.DictWriter(f, crypto.keys())
+                if f.tell() == 0:
+                    w.writeheader()
+                    w.writerow(crypto)
+                else:
+                    w.writerow(crypto)
+    time.sleep(300)
+
